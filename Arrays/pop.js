91aed74c851337233bb2a73c9addc7406c89c0a4
Neeraja <63630416+NeerajaSaragadam@users.noreply.github.com>
2021-12-28 11:51:06 +0530

    Update pop.js

--- Arrays/pop.js
+++ Arrays/pop.js
@@ -3,16 +3,3 @@ var found = example.pop()
 console.log(example)
 console.log(found)
 
-var obj
-var example1 = [obj = {
-    "id" : 1,
-    "name" : "Neeraja"
-},
-{
-"id": 2,
-"name" : 'Sowji'
-}
- ]
-
- example1.pop()
- console.log(example1)
