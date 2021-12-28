755f3961ba5eb3e5cebe21291bba405e1c08e496
Neeraja <63630416+NeerajaSaragadam@users.noreply.github.com>
2021-12-28 11:53:04 +0530

    Update unshift.js

--- Arrays/unshift.js
+++ Arrays/unshift.js
@@ -5,11 +5,3 @@ var ex1 = ex.unshift(6,7)
 console.log(ex)
 console.log(ex1)
 
-var obj
-
-var exobj = [obj = {
-    "id": 1,
-    "mame" : "Neeru"
-}]
-exobj.unshift({"id":2,"name":"Sowji"})
-console.log(exobj)
