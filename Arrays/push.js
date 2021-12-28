a9c10c3f90ab7dbc326f3249f8d32257a4f37a3b
Neeraja <63630416+NeerajaSaragadam@users.noreply.github.com>
2021-12-28 11:51:34 +0530

    Update push.js

--- Arrays/push.js
+++ Arrays/push.js
@@ -3,16 +3,3 @@ var example1 = example.push(7)
 console.log(example)
 console.log(example1)
 
-var obj
-
-var objArray = [obj = {
-    "id": 1,
-    "Name" : "Neeraja"
-},{
-    "id" : 2,
-    "Name" : "Sowji"
-}]
-
-objArray.push({"id":3,"Name":"Jasuu"})
-
-console.log(objArray)
