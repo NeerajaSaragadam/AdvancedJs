9e52310b67640bd45a1c98e00f420c87d8db8a49
Neeraja <63630416+NeerajaSaragadam@users.noreply.github.com>
2021-12-28 11:49:58 +0530

    Update map.js

--- Arrays/map.js
+++ Arrays/map.js
@@ -2,24 +2,3 @@ var ex = [1,2,3,4,5,67]
 var ex1 = ex.map(e => e+1)
 console.log(ex1)
 
-var obj 
-var obj1 = [obj = {
-    "id": 1,
-    "bookname": "apple",
-    "price" : 30
-},{
-    "id" : 2,
-    "bookname" : "orange",
-    "price" : 30
-},{
-    "id" : 3,
-    "bookname" : "grapes",
-    "price" : 50
-},{
-    "id" : "",
-    "bookname" : "",
-    "price" : ""
-}]
-
-var obj3 = obj1.filter(ob => ob.bookname !== "").map(ob => ob.bookname)
-console.log(obj3)
