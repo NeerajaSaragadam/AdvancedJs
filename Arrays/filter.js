8305a583b144b4f8142bc165bcffe547818bf05f
Neeraja <63630416+NeerajaSaragadam@users.noreply.github.com>
2021-12-28 11:49:06 +0530

    Update filter.js

--- Arrays/filter.js
+++ Arrays/filter.js
@@ -4,15 +4,3 @@
 var words = ["neeraja","laxmanRao","Jasuu","binu","bobby","tinku"]
 var ex = words.filter(word => word.length > 7 )
 console.log(ex,words)
-var obj 
-var objex = [obj = {
-   "id" : 1,
-   "name" : "Neeraja"
-},
-{
-    "id" : "",
-    "Name": ""
-}]
- var objex1 = objex.filter(ex => ex.id !== "")
-console.log(objex1)
-console.log(objex)
